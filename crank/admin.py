dbb335ed1963f8251f8dd7b43c1c498ae100a6b6
Tony Butcher <31665333+ab4359@users.noreply.github.com>
2017-10-28 16:09:48 -0400

    Add files via upload
    10/28 build - added database tables, error in linking auth_user table to Student_Course_Rating_Table

--- crank/admin.py
+++ crank/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Greeting, User
+from .models import Course_Listing_Table, Faculty_Table, Course_Faculty_Table, Student_Course_Rating_Table, Course_Rating_Table, Greeting, Profile, 
 
 # Register your models here.
+admin.site.register(Course_Listing_Table)
+admin.site.register(Faculty_Table)
+admin.site.register(Course_Faculty_Table)
+admin.site.register(Student_Course_Rating_Table)
+admin.site.register(Course_Rating_Table)
 admin.site.register(Greeting)
-admin.site.register(User)
+admin.site.register(Profile)
+
--- crank/models.py
+++ crank/models.py
@@ -1,11 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField('date created', auto_now_add=True)
+    courseid = models.DateTimeField('date created', auto_now_add=True)
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    email_confirmed = models.BooleanField(default=False)
+
+#begin Tony-Frank pair insert
+class Course_Listing_Table(models.Model):
+    courseid = models.IntegerField(primary_key=True)
+    coursenumber=models.CharField(max_length=10)
+    coursetitle=models.CharField(max_length=128)
+
+class Faculty_Table(models.Model):
+    facultyid = models.IntegerField(primary_key=True)
+    facultyname = models.CharField(max_length=128)
+
+class Course_Faculty_Table(models.Model):
+    course = models.ForeignKey(Course_Listing_Table)
+    faculty = models.ForeignKey(Faculty_Table)
+    course_faculty_pair = models.IntegerField(primary_key=True)
+
+class Student_Course_Rating_Table(models.Model):
+    #uni = models.ForeignKey(Student_Table) #need to fix this to match up with auth_user
+    uni = models.ForeignKey(auth_user)
+    #uni = models.IntegerField() #stub to remind Tony this is where the uni field should link to uni from account registration
+    class_id = models.ForeignKey(Course_Faculty_Table)
+    student_course = models.IntegerField(primary_key=True)
+
+class Course_Rating_Table(models.Model):
+    unique_id = models.ForeignKey(Student_Course_Rating_Table)
+    usefulness = models.IntegerField()
+    lecture_quality = models.IntegerField()
+    overall_quality = models.IntegerField()
+    oral_written_tests_helpful = models.IntegerField()
+    learned_much_info = models.IntegerField()
+
+#end Tony-Frank pair insert
+
+class Course(models.Model):
+    course_number = models.CharField(max_length = 30)
+    @classmethod
+    def create(cls, course_number):
+        course_number = cls(course_number=course_number)
+        # do something with the book
+        return course_number
 
 
-class User(models.Model):
-    name = models.CharField(max_length = 30, primary_key=True)
-    userName = models.CharField(max_length = 30)
-    password = models.CharField(max_length = 30)
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+    instance.profile.save()
+

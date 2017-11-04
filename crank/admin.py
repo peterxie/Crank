eaf4dc97de5dcd01729eca8cba6fc2734c258a76
Peter Xie <pxie8@bloomberg.net>
2017-11-04 17:25:04 -0400

    undid testing changes

--- crank/admin.py
+++ crank/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Course_Faculty_Table, Rating_id, Profile 
+from .models import Course_Listing_Table, Faculty_Table, Course_Faculty_Table, Rating_id, Profile 
 
+admin.site.register(Course_Listing_Table)
+admin.site.register(Faculty_Table)
 admin.site.register(Course_Faculty_Table)
 admin.site.register(Rating_id)
 admin.site.register(Profile)

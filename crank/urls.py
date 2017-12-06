e2ab0d36ba83dac12dd6197f2f9a919cbb2c4440
Tony Butcher <31665333+ab4359@users.noreply.github.com>
2017-12-05 20:57:40 -0800

    index.html append

--- crank/urls.py
+++ crank/urls.py
@@ -31,4 +31,4 @@ urlpatterns = [
     url(r'^delete_rank/(?P<delete_id>[0-9A-Za-z]+)/$', crank.views.delete_rank, name='delete_rank'),
     url(r'^search', crank.views.search, name='search'),
     url(r'^admin/', include(admin.site.urls)),
-]
+], + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
